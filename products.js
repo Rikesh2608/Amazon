2b8ad5f3f5c545b731ca99bc51b6a87ad55e1d80
Ricky26 <154548534+Rikesh2608@users.noreply.github.com>
2023-12-27 22:34:14 +0530

    Add files via upload

--- products.js
+++ products.js
@@ -98,5 +98,165 @@ export let products = [
         deliverycost:0,
         reviews: 89,
         image: 'knit-athletic-sneakers-gray.jpg'
+    },
+    {
+        id:11,
+        name: `Women's Chiffon Beachwear Cover Up - Black`,
+        rating: 45,
+        price: 2070,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 235,
+        image: 'women-chiffon-beachwear-coverup-black.jpg'
+    },
+    {
+        id:12,
+        name: 'Round Sunglasses',
+        rating: 45,
+        price: 1569,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 30,
+        image: 'round-sunglasses-black.jpg'
+    },
+    {
+        id:13,
+        name: `Women's Two Strap Buckle Sandals - Tan`,
+        rating: 45,
+        price: 2499,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 562,
+        image: 'women-beach-sandals.jpg'
+    },
+    {
+        id:14,
+        name: 'Blackout Curtains Set 4-Pack - Beige',
+        rating: 45,
+        price: 3399,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 232,
+        image: 'blackout-curtain-set-beige.webp'
+    },
+    {
+        id:15,
+        name: `Men's Slim-Fit Summer Shorts`,
+        rating: 4,
+        price: 1699,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 160,
+        image: 'men-slim-fit-summer-shorts-gray.jpg'
+    },
+    {
+        id:16,
+        name: 'Electric Glass and Steel Hot Tea Water Kettle - 1.7-Liter',
+        rating: 5,
+        price: 3074,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 846,
+        image: 'knit-athletic-sneakers-gray.jpg'
+    },
+    {
+        id:17,
+        name: `Ultra Soft Tissue 2-Ply - 18 Box`,
+        rating: 4,
+        price: 2374,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 99,
+        image: 'facial-tissue-2-ply-18-boxes.jpg'
+    },
+    {
+        id:18,
+        name: 'Straw Lifeguard Sun Hat',
+        rating: 4,
+        price: 2299,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 215,
+        image: 'straw-sunhat.webp'
+    },
+    {
+        id:19,
+        name: `Sterling Silver Sky Flower Stud Earrings`,
+        rating: 45,
+        price: 1799,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 52,
+        image: 'sky-flower-stud-earrings.webp'
+    },
+    {
+        id:20,
+        name: `Women's Stretch Popover Hoodie`,
+        rating: 5,
+        price: 1374,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 2465,
+        image: 'women-stretch-popover-hoodie-black.jpg'
+    },
+    {
+        id:21,
+        name: `Bathroom Bath Rug Mat 20 x 31 Inch - Grey`,
+        rating: 45,
+        price: 1250,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 119,
+        image: 'bathroom-rug.jpg'
+    },
+    {
+        id:22,
+        name: `Women's Knit Ballet Flat`,
+        rating: 4,
+        price: 3074,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 846,
+        image: 'women-knit-ballet-flat-black.jpg'
+    },
+    {
+        id:23,
+        name: `Men's Regular-Fit Quick-Dry Golf Polo Shirt`,
+        rating: 45,
+        price: 1599,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 2556,
+        image: 'men-golf-polo-t-shirt-blue.jpg'
+    },
+    {
+        id:24,
+        name: 'Trash Can with Foot Pedal - Brushed Stainless Steel',
+        rating: 45,
+        price: 8399,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 2286,
+        image: 'trash-can-with-foot-pedal-50-liter.jpg'
+    },
+    {
+        id:25,
+        name: `Duvet Cover Set with Zipper Closure`,
+        rating: 4,
+        price: 2399,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 456,
+        image: 'duvet-cover-set-blue-twin.jpg'
+    },
+    {
+        id:26,
+        name: `Women's Chunky Cable Beanie - Gray`,
+        rating: 5,
+        price: 1259,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 83,
+        image: 'women-chunky-beanie-gray.webp'
     }
 ];

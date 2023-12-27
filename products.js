d13b26f0aa75770c12dc4c1d3091941800ab8379
Ricky26 <154548534+Rikesh2608@users.noreply.github.com>
2023-12-27 10:59:23 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,102 @@
+export let products = [
+    {
+        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+        rating: 45,
+        price: 109,
+        quantity: 1,
+        reviews: 87,
+        image: 'athletic-cotton-socks-6-pairs.jpg',
+        deliverycost:0,
+        id:1
+    },
+    {
+        id:2,
+        name: 'Intermediate Size Basketball',
+        rating: 4,
+        price: 2095,
+        quantity: 1,
+        reviews: 127,
+        deliverycost:0,
+        image: 'intermediate-composite-basketball.jpg'
+    },
+    {
+        id:3,
+        name: 'Adults Plain Cotton T-Shirt-2-Pack',
+        rating: 45,
+        price: 799,
+        quantity: 1,
+        reviews: 56,
+        deliverycost:0,
+        image: 'adults-plain-cotton-tshirt-2-pack-teal.jpg'
+    },
+    {
+        id:4,
+        name: '2 Slot Toaster - Black',
+        rating: 5,
+        price: 1899,
+        quantity: 1,
+        reviews: 2196,
+        deliverycost:0,
+        image: 'black-2-slot-toaster.jpg'
+    },
+    {
+        id:5,
+        name: '6 Piece White Dinner Plate Set',
+        rating: 4,
+        price: 2067,
+        deliverycost:0,
+        quantity: 1,
+        reviews: 37,
+        image: '6-piece-white-dinner-plate-set.jpg'
+    },
+    {
+        id:6,
+        name: '6-Piece Nonstick, Carbon Steel Oven Bakeware Baking',
+        rating: 45,
+        price: 3499,
+        quantity: 1,
+        reviews: 175,
+        deliverycost:0,
+        image: '6-piece-non-stick-baking-set.webp'
+    },
+    {
+        id:7,
+        name: 'Plain Hooded Fleece Sweatshirt',
+        rating: 45,
+        deliverycost:0,
+        price: 2400,
+        quantity: 1,
+        reviews: 317,
+        image: 'plain-hooded-fleece-sweatshirt-yellow.jpg'
+    },
+    {
+        id:8,
+        name: 'Luxury Towel Set - Graphite Gray',
+        rating: 45,
+        price: 3599,
+        deliverycost:0,
+        quantity: 1,
+        reviews: 144,
+        image: 'luxury-tower-set-6-piece.jpg'
+    },
+    {
+        id:9,
+        name: 'Liquid Laundry Detergent, 110 Loads,',
+        rating: 45,
+        price: 2899,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 305,
+        image: 'liquid-laundry-detergent-plain.jpg'
+    },
+    {
+        id:10,
+        name: 'Waterproof Knit Athletic Sneakers - Gray',
+        rating: 45,
+        price: 3399,
+        quantity: 1,
+        deliverycost:0,
+        reviews: 89,
+        image: 'knit-athletic-sneakers-gray.jpg'
+    }
+];
